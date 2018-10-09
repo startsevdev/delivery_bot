2615e71e4fa870b0e7df5fb757d51f2fef7f757e
Startsev <startsevdev@gmail.com>
2018-10-09 23:07:04 +0300

    3

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ print("\n- - - StartsevDev's IskraPizzaBot - - -\n")
 
 token = tokens.IskraPizzaBot
 bot = telebot.TeleBot(token)
-logger = telebot.logger
 
-items = ["Маргарита", "Мортадела", "Прошуто фунге", "Пепперони", "4 сыра", "С грушей и горгонзоллой",
-         "Вегетарианская", "С пореем", "С беконом и маскарпоне", "С лососем", "С артишоком и анчоусом",
-         "С лисичками и беконом", "Со старчетеллой и инжиром", "Сациви кальцоне", "Ди парма", "Фокачча"]
+items = ["Маргарита", "Прошуто фунге", "Пепперони"]
+
+#, "Мортадела", "4 сыра", "С грушей и горгонзоллой",
+#         "Вегетарианская", "С пореем", "С беконом и маскарпоне", "С лососем", "С артишоком и анчоусом",
+#         "С лисичками и беконом", "Со старчетеллой и инжиром", "Сациви кальцоне", "Ди парма", "Фокачча
 
 
 def console_print(message):
@@ -49,28 +50,73 @@ def pre_order_menu_keyboard():
     for item in items:
         keyboard.add(item)
 
+    keyboard.add("Удалить пиццу")
+
     return keyboard
 
 
-def pre_order_keyboard_1():
+def item_keyboard_1():
     keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    keyboard.add("Добавить")
+    keyboard.add("Добавить в заказ")
     keyboard.add("Меню")
 
     return keyboard
 
 
-def pre_order_keyboard_2():
+def item_keyboard_2():
     keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    keyboard.add("Добавить")
+    keyboard.add("Добавить в заказ")
     keyboard.add("Меню")
     keyboard.add("Оформить заказ")
 
     return keyboard
 
 
+def confirm_order_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    keyboard.add("Подтвердить заказ")
+    keyboard.add("Добавить пиццу")
+    keyboard.add("Удалить пиццу")
+
+    return keyboard
+
+
 
 #WORK WITH DATABASE
+
+
+def return_order_list(message):
+    order_list = "Мой заказ: \n"
+    order_item_names = []
+
+    conn = sqlite3.connect('/Users/alexander/code/databases/iskra.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT item_id FROM users_items WHERE user_id = {}".format(message.from_user.id))
+    order_item_ids = cursor.fetchall()
+    print(order_item_ids)
+
+    for i in order_item_ids:
+        cursor.execute("SELECT name FROM items WHERE id = {}".format(i[0]))
+        order_item_names.append(cursor.fetchone()[0])
+
+    conn.close()
+
+    for counter, item_name in enumerate(order_item_names[0:], 1):
+        order_list += "{}. {}\n".format(counter, item_name)
+
+    if order_list == "Мой заказ: \n":
+        order_list = "Заказ пуст 🤷‍♀️"
+
+    return order_list
+
+
+def set_state(message, state):
+    conn = sqlite3.connect('/Users/alexander/code/databases/iskra.db')
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET state = '{}' WHERE user_id = {}".format(state, message.from_user.id))
+    conn.commit()
+    conn.close()
 
 
 def return_state(message):
@@ -118,13 +164,32 @@ def add_item_in_order(message):
     cursor.execute("SELECT id from items WHERE name = '{}'".format(item_name))
     item_id = cursor.fetchone()[0]
 
-    cursor.execute("INSERT INTO users_items VALUES ({}, {})".format(message.from_user.id, item_id))
+    cursor.execute("INSERT INTO users_items VALUES ({}, {}, Null)".format(message.from_user.id, item_id))
     conn.commit()
 
     cursor.execute("UPDATE users SET state = 'WAIT ORDER' WHERE user_id = {}".format(message.from_user.id))
     conn.commit()
 
     conn.close()
+
+
+def del_item_from_order(message):
+    conn = sqlite3.connect('/Users/alexander/code/databases/iskra.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM users_items WHERE user_id = {}".format(message.from_user.id))
+    id = cursor.fetchall()[int(message.text)][0]
+    cursor.execute("DELETE FROM users_items WHERE id = {}".format(id))
+
+    conn.commit()
+    conn.close()
+
+
+def send_menu(message):
+    if return_state(message) == "WAIT_FIRST_ITEM":
+        bot.send_message(message.from_user.id, "Выберите пиццу: ", reply_markup=menu_keyboard())
+    else:
+        bot.send_message(message.from_user.id, "Выберите пиццу: ", reply_markup=pre_order_menu_keyboard())
 
 
 @bot.message_handler(commands=['start'])
@@ -141,35 +206,44 @@ def giving_text(message):
     console_print(message)
 
     if message.text == "Меню":
-        if return_state(message) == "WAIT_FIRST_ITEM":
-            bot.send_message(message.from_user.id, "Выберите пиццу: ", reply_markup=menu_keyboard())
-        else:
-            bot.send_message(message.from_user.id, "Выберите пиццу: ", reply_markup=pre_order_menu_keyboard())
+        send_menu(message)
 
-    elif message.text == "Добавить":
+    elif message.text == "Добавить в заказ":
         add_item_in_order(message)
 
-        bot.send_message(message.from_user.id, "Пицца добавлена в заказ!", reply_markup=pre_order_menu_keyboard())
+        bot.send_message(message.from_user.id, "Пицца добавлена!\n\n" + return_order_list(message), reply_markup=pre_order_menu_keyboard())
 
     elif message.text == "Оформить заказ":
-        order_text = "Мой заказ: \n"
-        order_items = []
 
         conn = sqlite3.connect('/Users/alexander/code/databases/iskra.db')
         cursor = conn.cursor()
-
-        cursor.execute("SELECT item_id FROM users_items WHERE user_id = {}".format(message.from_user.id))
-        item_ids = cursor.fetchall()
-
-        for i in item_ids:
-            cursor.execute("SELECT name FROM items WHERE id = {}".format(i[0]))
-            order_items.append(cursor.fetchone()[0])
-
-        print(order_items)
-
+        cursor.execute("UPDATE users SET state = 'WAIT_CONFIRM' WHERE user_id = {}".format(message.from_user.id))
         conn.commit()
         conn.close()
 
+        bot.send_message(message.from_user.id, return_order_list(message), reply_markup=confirm_order_keyboard())
+
+    elif message.text == "Добавить пиццу":
+        send_menu(message)
+
+    elif message.text == "Удалить пиццу":
+        if return_order_list(message) == "Список пуст 🤷‍♀️":
+            send_menu(message, return_order_list(message))
+        else:
+            bot.send_message(message.chat.id, return_order_list(message))
+            bot.send_message(message.chat.id, "Укажите номер пунктa")
+
+            set_state(message, "WAIT_DEL")
+
+    elif return_state(message) == "WAIT_DEL":
+        if message.text.isdigit() and int(message.text) > 0:
+            del_item_from_order(message)
+            set_state(message, "WAIT_CONFIRM")
+            bot.send_message(message.from_user.id, return_order_list(message), reply_markup=confirm_order_keyboard())
+        else:
+            bot.send_message(message.from_user.id, "❗ Введите натуральное число.")
+
+    # message.text == one of ITEM NAMES
     else:
         for item in items:
             if message.text == item:
@@ -191,15 +265,9 @@ def giving_text(message):
                 bot.send_photo(message.from_user.id, image)
 
                 if return_state(message) == "WAIT_FIRST_ITEM":
-                    bot.send_message(message.from_user.id, str(price) + " р.", reply_markup=pre_order_keyboard_1())
+                    bot.send_message(message.from_user.id, str(price) + " р.", reply_markup=item_keyboard_1())
                 else:
-                    bot.send_message(message.from_user.id, str(price) + " р.", reply_markup=pre_order_keyboard_2())
+                    bot.send_message(message.from_user.id, str(price) + " р.", reply_markup=item_keyboard_2())
 
 
-bot.polling(none_stop=True)
-
-#while True:
-#    try:
-#        bot.polling(none_stop=True)   except Exception as e:
-#        logger.error(e)
-#        time.sleep(15)
+bot.polling()
